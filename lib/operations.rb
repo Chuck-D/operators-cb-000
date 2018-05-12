877ba129740697b4a54aa184bfd30b20f4858332
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-12 22:44:42 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,10 @@
 def unsafe?(speed)
   if speed<40 || speed >60
     return true
-=begin  else
+  else
     return false
-=end    
+  end
 end
-
-end
-
-
 
 def not_safe?(speed)
   speed<40 || speed >60 ? true:false
